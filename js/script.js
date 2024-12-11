32c86f7994f138cd339cd2f0ba4e66150b9cbe0a
MasterCleanHQ <John@mastercleanhq.com>
2024-12-11 14:16:49 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,119 +2,129 @@
 // Smart Resize
 ///////////////////////////////
 
-(function ($, sr) {
-  var debounce = function (func, threshold, execAsap) {
-    var timeout;
-    return function debounced() {
-      var obj = this,
-        args = arguments;
-      function delayed() {
-        if (!execAsap) func.apply(obj, args);
-        timeout = null;
-      }
-      if (timeout) clearTimeout(timeout);
-      else if (execAsap) func.apply(obj, args);
-      timeout = setTimeout(delayed, threshold || 100);
+(function($, sr) {
+    var debounce = function(func, threshold, execAsap) {
+        var timeout;
+        return function debounced() {
+            var obj = this,
+                args = arguments;
+            function delayed() {
+                if (!execAsap) func.apply(obj, args);
+                timeout = null;
+            }
+            if (timeout) clearTimeout(timeout);
+            else if (execAsap) func.apply(obj, args);
+            timeout = setTimeout(delayed, threshold || 100);
+        };
     };
-  };
 
-  // smartresize
-  jQuery.fn[sr] = function (fn) {
-    return fn ? this.bind("resize", debounce(fn)) : this.trigger(sr);
-  };
-})(jQuery, "smartresize");
+    // smartresize
+    jQuery.fn[sr] = function(fn) {
+        return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr);
+    };
+})(jQuery, 'smartresize');
 
-$(function () {
-  ///////////////////////////////
-  // Fix the Home Height
-  ///////////////////////////////
+$(function() {
+    ///////////////////////////////
+    // Fix the Home Height
+    ///////////////////////////////
+    var setHomeBannerHeight = function() {
+        var homeHeight = $(window).height();
+        $('#overlay-1').height(homeHeight);
+    };
 
-  var setHomeBannerHeight = function () {
-    var homeHeight = $(window).height();
-    $("#overlay-1").height(homeHeight);
-  };
-
-  setHomeBannerHeight();
-
-  ///////////////////////////////
-  // One Page Smooth Scrolling
-  ///////////////////////////////
-
-  $("a[href*=#]:not([href=#])").click(function () {
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length
-        ? target
-        : $("[name=" + this.hash.slice(1) + "]");
-      if (target.length) {
-        $("html,body").animate(
-          {
-            scrollTop: target.offset().top,
-          },
-          1000
-        );
-        return false;
-      }
-    }
-  });
-
-  ///////////////////////////////
-  // Center Home Slideshow Text
-  ///////////////////////////////
-
-  function centerHomeBannerText() {
-    var bannerText = jQuery("#wrapper .starting-text");
-    var bannerTextTop =
-      jQuery("#wrapper").actual("height") / 2 -
-      jQuery("#wrapper .starting-text").actual("height") / 2 -
-      20;
-    bannerText.css("padding-top", bannerTextTop + "px");
-    bannerText.show();
-  }
-
-  centerHomeBannerText();
-
-  jQuery(window).smartresize(function () {
     setHomeBannerHeight();
+
+    ///////////////////////////////
+    // One page Smooth Scrolling
+    ///////////////////////////////
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate(
+                    {
+                        scrollTop: target.offset().top
+                    },
+                    1000
+                );
+                return false;
+            }
+        }
+    });
+
+    ///////////////////////////////
+    // Center Home Slideshow Text
+    ///////////////////////////////
+    function centerHomeBannerText() {
+        var bannerText = jQuery('#wrapper .starting-text');
+        var bannerTextTop =
+            jQuery('#wrapper').actual('height') / 2 -
+            jQuery('#wrapper .starting-text').actual('height') / 2 -
+            20;
+        bannerText.css('padding-top', bannerTextTop + 'px');
+        bannerText.show();
+    }
+
     centerHomeBannerText();
-  });
+
+    jQuery(window).smartresize(function() {
+        setHomeBannerHeight();
+        centerHomeBannerText();
+    });
+});
+
+$(document).ready(function() {
+    ///////////////////////////////
+    // Initialize WOW.js for animations
+    ///////////////////////////////
+    new WOW().init();
+
+    ///////////////////////////////
+    // Initialize Owl Carousel
+    ///////////////////////////////
+    $('#client-speech').owlCarousel({
+        autoPlay: 3000,
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 700,
+        paginationSpeed: 1000,
+        singleItem: true
+    });
+
+    ///////////////////////////////
+    // Fix Home Banner Height on Resize
+    ///////////////////////////////
+    var setHomeBannerHeight = function() {
+        var homeHeight = $(window).height();
+        $('#overlay-1').height(homeHeight);
+    };
+    setHomeBannerHeight();
 });
 
 ///////////////////////////////
-// Scroll Behavior
+// Navbar Scroll Behavior
 ///////////////////////////////
 
-$(document).ready(function () {
-  new WOW().init();
+$(document).ready(function() {
+    var navbar = $('#navigation > .navbar'); // Target the navbar
+    var scrollOffset = $('#wrapper').offset().top + $('#wrapper').height();
 
-  $("#client-speech").owlCarousel({
-    autoPlay: 3000,
-    navigation: false, // Show next and prev buttons
-    slideSpeed: 700,
-    paginationSpeed: 1000,
-    singleItem: true,
-  });
-
-  var menu = $("#navigation > .navbar");
-  var origOffsetY = $("#overlay-1").height(); // Adjusted to match the background height
-
-  function scroll() {
-    if ($(window).scrollTop() > origOffsetY) {
-      menu.addClass("navbar-scrolled"); // Add your new class
-      menu.removeClass("navbar-white");
-    } else {
-      menu.addClass("navbar-white");
-      menu.removeClass("navbar-scrolled"); // Remove your new class
+    // Function to handle scroll
+    function handleScroll() {
+        if ($(window).scrollTop() > scrollOffset) {
+            navbar.addClass('navbar-scrolled'); // Add scrolled class
+            navbar.removeClass('navbar-white'); // Remove white class
+        } else {
+            navbar.addClass('navbar-white'); // Add white class
+            navbar.removeClass('navbar-scrolled'); // Remove scrolled class
+        }
     }
-  }
 
-  // Attach scroll event
-  $(document).on("scroll", scroll);
-
-  // Trigger scroll function to apply the initial state
-  scroll();
+    // Trigger scroll function on page load and scroll
+    $(window).on('scroll', handleScroll);
+    handleScroll(); // Run it once on load in case page is already scrolled
 });
